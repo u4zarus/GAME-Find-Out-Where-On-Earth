6da3531b5efd2164b8d1fa4250385531e5cb6424
Stepan Manaenko <u4zarus@gmail.com>
2024-03-18 15:56:44 +0100

    config change

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
-    output: "export",
+    images: {
+        loader: "akamai",
+        path: "",
+    },
+    assetPrefix: "./",
 };
 
 export default nextConfig;
