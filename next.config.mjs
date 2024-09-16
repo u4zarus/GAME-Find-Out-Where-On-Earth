6faa646f76c4e484b002eba6685d224937f0510c
Stepan Manaenko <u4zarus@gmail.com>
2024-09-16 18:14:42 +0200

    4

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+    output: "export",
     assetPrefix: "/GAME-Find-Out-Where-On-Earth",
     basePath: "/GAME-Find-Out-Where-On-Earth",
     images: {
