fc096062811dce7d47d1d4de39e8ca3a92dec47d
Stepan Manaenko <u4zarus@gmail.com>
2024-12-09 18:06:02 +0100

    tt

--- src/app/api/users/login/route.js
+++ src/app/api/users/login/route.js
@@ -34,22 +34,7 @@ export async function POST(request) {
             username: user.username,
             // email: user.email,
         };
-        const token = jwt.sign(tokenData, process.env.TOKEN_SECRET, {
-            expiresIn: "1d",
-        });
-
-        if (token) {
-            try {
-                jwt.verify(token, process.env.TOKEN_SECRET);
-            } catch (error) {
-                response.cookies.delete("token", {
-                    path: "/",
-                    sameSite: "lax",
-                    secure: process.env.NODE_ENV === "production",
-                });
-                return NextResponse.redirect(new URL("/login", request.url));
-            }
-        }
+        const token = jwt.sign(tokenData, process.env.TOKEN_SECRET);
 
         const response = NextResponse.json({
             message: "Login successful",
